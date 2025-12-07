d0292b40ce7066ee60f34af79272a8d38f171694
Théo Peuchlestrade <83510973+theopeuchlestrade@users.noreply.github.com>
2025-12-07 21:36:32 +0100

    feat: ajouter l'authentification OAuth avec Google et Apple (#22)

--- lib/src/core/config.dart
+++ lib/src/core/config.dart
@@ -3,10 +3,27 @@ const String apiBaseUrl = String.fromEnvironment(
   defaultValue: 'http://127.0.0.1:8080',
 );
 
+const String googleWebClientId = String.fromEnvironment(
+  'FIESTAAA_GOOGLE_WEB_CLIENT_ID',
+  defaultValue:
+      '900475997784-dlhlkhvo70sk0jpi72n32ugp85cq7etf.apps.googleusercontent.com',
+);
+
+const String appleServiceId = String.fromEnvironment(
+  'FIESTAAA_APPLE_SERVICE_ID',
+  defaultValue: '',
+);
+
+const String appleRedirectUri = String.fromEnvironment(
+  'FIESTAAA_APPLE_REDIRECT_URI',
+  defaultValue: '',
+);
+
 // Public VAPID key for FCM web push
 const String fcmWebVapidKey = String.fromEnvironment(
   'FIESTAAA_FCM_VAPID_KEY',
-  defaultValue: 'BCyVeS1aOKK5gZLD7lKtr6U637mA5c3CZJCKle9jQdNEXsHT8drtq-2cC22vJ67OVHK82Epuefd8nqsV5NfPiKI',
+  defaultValue:
+      'BCyVeS1aOKK5gZLD7lKtr6U637mA5c3CZJCKle9jQdNEXsHT8drtq-2cC22vJ67OVHK82Epuefd8nqsV5NfPiKI',
 );
 
 Uri buildWsUri(String path, {Map<String, String>? queryParameters}) {
