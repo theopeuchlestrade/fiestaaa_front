ea421e282df539dd438603636bbe1fc210f1f1b1
Théo Peuchlestrade <theo.peuchlestrade@gmail.com>
2025-12-07 22:54:18 +0100

    feat: ajouter la configuration Firebase et le service worker pour les notifications

--- web/firebase-messaging-sw.template.js
+++ web/firebase-messaging-sw.template.js
@@ -1,3 +1,14 @@
+// Firebase Messaging service worker template.
+// Values are filled from your .env file via tool/generate_firebase_sw.dart.
+
+self.FIREBASE_PROJECT_ID = "{{FIREBASE_PROJECT_ID}}";
+self.FIREBASE_STORAGE_BUCKET = "{{FIREBASE_STORAGE_BUCKET}}";
+self.FIREBASE_MESSAGING_SENDER_ID = "{{FIREBASE_MESSAGING_SENDER_ID}}";
+self.FIREBASE_WEB_API_KEY = "{{FIREBASE_WEB_API_KEY}}";
+self.FIREBASE_WEB_APP_ID = "{{FIREBASE_WEB_APP_ID}}";
+self.FIREBASE_WEB_MEASUREMENT_ID = "{{FIREBASE_WEB_MEASUREMENT_ID}}";
+self.FIREBASE_AUTH_DOMAIN = "{{FIREBASE_AUTH_DOMAIN}}";
+
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
